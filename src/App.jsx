acbd679e6b1b28462fd5532bdd906f0593a3db4d
Anuj Gupta <129574489+anujgupta8519@users.noreply.github.com>
2024-01-22 20:39:39 +0530

    Todo roject using React redux done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+<div className=''>
+<AddTodo/>
+<Todos/>
+</div>
+  )
+}
+
+export default App
